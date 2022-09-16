f5a5f39fd01714f5f23cd5b2a184e8966a3217ac
dongjun0128 <dongjun9701@naver.com>
2022-09-16 17:26:08 +0900

    DFS / BFS 추가

---
+++ Python/DFS.py
@@ -0,0 +1,30 @@
+# DFS 메서드 정의
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if visited[i] == False:
+            dfs(graph, i, visited)
+
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+# 0번은 쓰지 않으므로 1개 더 추가한 리스트를 만들어준다.
+visited = [False] * 9
+
+# 정의된 DFS 함수 호출
+dfs(graph, 1, visited)
